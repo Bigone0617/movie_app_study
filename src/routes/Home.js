c74952e089712a986c44252a8f0d02750fd3f759
Bigone0617 <xodlfsha@gmail.com>
2021-03-19 16:14:07 +0900

    searching movie

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -6,7 +6,8 @@ import axios from "axios";
 class Home extends React.Component{
   state = {
     isLoading: true,
-    movies : []
+    movies : [],
+    searchMovie: undefined
   }
 
   getMovies = async () => {
@@ -19,12 +20,16 @@ class Home extends React.Component{
     this.setState({movies, isLoading : false});
   }
 
+  searchingMovie = (e) => {
+    this.setState({searchMovie : e.target.value});
+  }
+
   componentDidMount(){
     this.getMovies();
   }
 
   render(){
-    const {isLoading, movies} = this.state;
+    const {isLoading, movies, searchMovie} = this.state;
 
     return (
       <div>
@@ -32,17 +37,51 @@ class Home extends React.Component{
           <div>Loading....</div>
         ) : (
           <div>
-            {movies.map((movie, index) => (
-              <Movie 
-                key = {index} 
-                id = {movie.id}
-                title = {movie.title}
-                year = {movie.year}
-                summary = {movie.summary}
-                poster = {movie.medium_cover_image}
-                genres = {movie.genres}
+            <div>
+              <input 
+                placeholder="영화명"
+                value={searchMovie}
+                onChange={this.searchingMovie}
               />
-            ))}
+            </div>
+            <div>
+              {(searchMovie !== undefined && searchMovie !== "") ? (
+                <div>
+                  {movies.map((movie, index) => {
+                    if(movie.title.toLowerCase().includes(this.state.searchMovie.toLowerCase())){
+                      return (
+                        <Movie 
+                          key = {index} 
+                          id = {movie.id}
+                          title = {movie.title}
+                          year = {movie.year}
+                          summary = {movie.summary}
+                          poster = {movie.medium_cover_image}
+                          genres = {movie.genres}
+                        />
+                      )
+                    }
+                  })
+
+                  }
+                </div>
+              ):(
+                <div>
+                  {movies.map((movie, index) => (
+                    <Movie 
+                      key = {index} 
+                      id = {movie.id}
+                      title = {movie.title}
+                      year = {movie.year}
+                      summary = {movie.summary}
+                      poster = {movie.medium_cover_image}
+                      genres = {movie.genres}
+                    />
+                  ))}
+                </div>
+              )}
+              
+            </div>
           </div>
         )}
       </div>
